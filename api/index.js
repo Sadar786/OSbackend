288338d8c99f5480a5a8526b4bb04c069941ba3f
sadar786 <bathoorsadarullah@gmail.com>
2025-10-29 20:10:03 +0500

    index.js corrected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 // oceanStella/api/index.js
 const serverless = require('serverless-http');
-const app = require('../server/server'); // exports Express app when VERCEL is set
+const app = require('../server.js'); // exports Express app when VERCEL is set
 
 const handler = serverless(app);
 module.exports = async (req, res) => handler(req, res);
